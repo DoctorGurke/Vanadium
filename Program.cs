ddd93c6019639e90d280af25b7f3e6c5496603b1
Doctor Gurke <docgurk@gmail.com>
2022-03-03 00:57:07 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+
+namespace Vanadium;
+
+public class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("Vanadium Renderer");
+        Console.Read();
+    }
+}
