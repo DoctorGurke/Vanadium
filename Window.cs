65a139473e77ada77792b91a4ab2c7d171d896d9
Doctor Gurke <docgurk@gmail.com>
2022-03-03 15:28:53 +0100

    draw a rectangle using indices

--- Window.cs
+++ Window.cs
@@ -10,14 +10,23 @@ public class Window : GameWindow {
 
 	private readonly float[] _vertices =
 	{
-		-0.5f, -0.5f, 0.0f, // Bottom-left vertex
-            0.5f, -0.5f, 0.0f, // Bottom-right vertex
-            0.0f,  0.5f, 0.0f  // Top vertex
+		0.5f,  0.5f, 0.0f, // top right
+        0.5f, -0.5f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f, // top left
+    };
+
+	private readonly uint[] _indices =
+	{
+        0, 1, 3, // The first triangle will be the bottom-right half of the triangle
+        1, 2, 3  // Then the second will be the top-right half of the triangle
     };
 
 	private int _vertexBufferObject;
 
 	private int _vertexArrayObject;
+
+	private int _elementBufferObject;
 
 	private Shader _shader;
 
@@ -26,19 +35,21 @@ public class Window : GameWindow {
 
 		GL.ClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
-		// create vbo
+		// create, bind and populate vbo
 		_vertexBufferObject = GL.GenBuffer();
-		// bind vbo
 		GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
-		// populate vbo
 		GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
 
 		_vertexArrayObject = GL.GenVertexArray();
 		GL.BindVertexArray(_vertexArrayObject);
 
 		GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
-		// Enable variable 0 in the shader.
 		GL.EnableVertexAttribArray(0);
+
+		// create, bind and populate ebo
+		_elementBufferObject = GL.GenBuffer();
+		GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
+		GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
 
 		_shader = new Shader("shaders/generic.vert", "shaders/generic.frag");
 		_shader.Use();
@@ -53,7 +64,7 @@ public class Window : GameWindow {
 
 		GL.BindVertexArray(_vertexArrayObject);
 
-		GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+		GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
 		
 		SwapBuffers();
 	}
