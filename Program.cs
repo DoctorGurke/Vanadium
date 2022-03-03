de543cb7599d89bc64f320e56de96f0423dac187
Doctor Gurke <docgurk@gmail.com>
2022-03-03 01:12:24 +0100

    the first window

--- Program.cs
+++ Program.cs
@@ -2,11 +2,23 @@
 global using System.Collections.Generic;
 global using System.Linq;
 
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.Common;
+using OpenTK.Mathematics;
+
 namespace Vanadium;
 
 public class Program {
     public static void Main(string[] args) {
-        Console.WriteLine("Vanadium Renderer");
-        Console.Read();
-    }
+		// init the settings for our main window
+		var nativeWindowSettings = new NativeWindowSettings() {
+			Size = new Vector2i(1920, 1080),
+			Title = "Vanadium Renderer"
+		};
+
+		// init and run our window type
+		using(var window = new Window(GameWindowSettings.Default, nativeWindowSettings)) {
+			window.Run();
+		}
+	}
 }
---
+++ Window.cs
@@ -0,0 +1,18 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace Vanadium;
+
+public class Window : GameWindow {
+	public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings) { }
+
+	protected override void OnUpdateFrame(FrameEventArgs args) {
+		base.OnUpdateFrame(args);
+
+		// close the window when ESC is pressed down
+		if(KeyboardState.IsKeyDown(Keys.Escape)) {
+			Close();
+		}
+	}
+}
