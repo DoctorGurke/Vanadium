e20a81038ec34a0a2024751d46589b0cdec75e6f
Doctor Gurke <docgurk@gmail.com>
2022-03-04 19:48:32 +0100

    extension documentation

--- common/math/MathX.cs
+++ common/math/MathX.cs
@@ -8,14 +8,29 @@ public static class MathX {
 
 	internal const float toRadians = 0.0174533f;
 
+	/// <summary>
+	/// Utility function to convert Degrees to Radians.
+	/// </summary>
+	/// <param name="f">Float Extension.</param>
+	/// <returns>The Radian value.</returns>
 	public static float DegreeToRadian(this float f) {
 		return f * toRadians;
 	}
 
+	/// <summary>
+	/// Utility function to convert Radians to Degrees.
+	/// </summary>
+	/// <param name="f">Float Extension.</param>
+	/// <returns>The Degree value.</returns>
 	public static float RadianToDegree(this float f) {
 		return f * toDegrees;
 	}
 
+	/// <summary>
+	/// Sort two floats by their size, min = a, max = b.
+	/// </summary>
+	/// <param name="a">ref float a</param>
+	/// <param name="b">ref float b</param>
 	private static void Order(ref float a, ref float b) {
 		if(!(a <= b)) {
 			float num = a;
@@ -24,8 +39,15 @@ public static class MathX {
 		}
 	}
 
-	public static float Clamp(this float v, float min, float max) {
+	/// <summary>
+	/// Clamp a float to a min and max
+	/// </summary>
+	/// <param name="v">Float Extension.</param>
+	/// <param name="min">The minimum allowed value.</param>
+	/// <param name="max">The maximum allowed value.</param>
+	/// <returns>The clamped value.</returns>
+	public static float Clamp(this float f, float min, float max) {
 		Order(ref min, ref max);
-		return (v < min) ? min : ((v < max) ? v : max);
+		return (f < min) ? min : ((f < max) ? f : max);
 	}
 }
--- common/util/StringX.cs
+++ common/util/StringX.cs
@@ -1,6 +1,13 @@
 ﻿namespace Vanadium;
 
 public static class StringX {
+
+	/// <summary>
+	/// Utility function to clean strings for common usage.
+	/// </summary>
+	/// <remarks>Will trim the string, replace '\' with '/' and remove newline and carry return characters.</remarks>
+	/// <param name="s">String extension.</param>
+	/// <returns>The cleaned st ring.</returns>
 	public static string Clean(this string s) {
 		return s.Trim().Replace("\\", "/").Replace("\n", "").Replace("\r", "");
 	}
