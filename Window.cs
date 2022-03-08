f73c12249f3071fa46e847ed804402425b03af75
Doctor Gurke <docgurk@gmail.com>
2022-03-08 18:07:59 +0100

    rotate top and bottom skybox faces properly

--- Window.cs
+++ Window.cs
@@ -41,7 +41,7 @@ public class Window : GameWindow {
 
 		Model.Precache("models/error.fbx");
 
-		Skybox.Load("materials/skybox/skybox_debug.vanmat");
+		Skybox.Load("materials/skybox/skybox01.vanmat");
 
 		new SceneObject {
 			Model = Model.Load("models/brickwall.fbx"),
