621f33243b1b8160b5fb1a3b81cbcba12bd2e818
Doctor Gurke <docgurk@gmail.com>
2022-03-09 19:07:26 +0100

    remove old comments for size, we use a struct for this now

--- shaders/common/common.glsl
+++ shaders/common/common.glsl
@@ -1,9 +1,9 @@
 ﻿layout (std140) uniform PerViewUniformBuffer {
-    layout(row_major) mat4 g_matWorldToProjection;  // 64
-    layout(row_major) mat4 g_matWorldToView;        // 64
-    vec3 g_vCameraPositionWs;                       // 12
-    vec3 g_vCameraDirWs;                            // 12
-    float g_flTime;                                 // 4
+    layout(row_major) mat4 g_matWorldToProjection;
+    layout(row_major) mat4 g_matWorldToView;
+    vec3 g_vCameraPositionWs;
+    vec3 g_vCameraDirWs;
+    float g_flTime;
 };
 
 vec4 tex2D(sampler2D tex, vec2 uv) {
