b80bd94d77a747d27538b0fa048e1c58a550d4dc
Doctor Gurke <docgurk@gmail.com>
2022-03-11 21:44:35 +0100

    add Plane struct

---
+++ common/math/Plane.cs
@@ -0,0 +1,17 @@
+﻿namespace Vanadium;
+
+/// <summary>
+/// Represents a simple Plane in 3D space
+/// </summary>
+public struct Plane
+{
+	/// <summary>
+	/// Distance from world origin to the nearest point on the plane
+	/// </summary>
+	public float distance;
+
+	/// <summary>
+	/// The normal of the plane
+	/// </summary>
+	public Vector3 normal;
+}
