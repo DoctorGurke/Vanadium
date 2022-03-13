5a1982cde1086c33d39599dd9c723ef727375493
Doctor Gurke <docgurk@gmail.com>
2022-03-13 21:40:16 +0100

    Update Transform.cs

--- common/math/Transform.cs
+++ common/math/Transform.cs
@@ -8,13 +8,28 @@ public struct Transform
 	public Rotation Rotation;
 	public float Scale;
 	public Matrix4 TransformMatrix => GetTransformMatrix();
+	public Matrix4 TranslationMatrix => Matrix4.CreateTranslation( Position );
+	public Matrix4 RotationMatrix => Rotation.Matrix;
+	public Matrix4 ScaleMatrix => Matrix4.CreateScale( Scale );
+
+	public static implicit operator Matrix4( Transform transform )
+	{
+		return transform.TransformMatrix;
+	}
+
+	public Transform(Vector3 pos, Rotation rot, float scale)
+	{
+		Position = pos;
+		Rotation = rot;
+		Scale = scale;
+	}
 
 	private Matrix4 GetTransformMatrix()
 	{
 		var transform = Matrix4.Identity;
-		transform *= Matrix4.CreateScale( Scale );
+		transform *= ScaleMatrix;
 		transform *= Rotation.Matrix;
-		transform *= Matrix4.CreateTranslation( Position );
+		transform *= TranslationMatrix;
 
 		return transform;
 	}
