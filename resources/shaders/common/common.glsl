7aaf3fbea7141e2e15fbb9bde70c2f3b005a880b
DoctorGurke <docgurk@gmail.com>
2022-04-15 22:49:59 +0200

    split material structs into separate include

--- resources/shaders/common/common.glsl
+++ resources/shaders/common/common.glsl
@@ -34,6 +34,8 @@ struct DirLight {
 #define MAX_SPOT_LIGHTS 64
 #define MAX_DIR_LIGHTS 16
 
+#include shaders/common/material.glsl
+
 layout (std140) uniform PerViewLightingUniformBuffer {
     vec4 g_vAmbientLightingColor;
     int g_nNumPointlights;
@@ -42,21 +44,4 @@ layout (std140) uniform PerViewLightingUniformBuffer {
     PointLight[MAX_POINT_LIGHTS] g_PointLights;
     SpotLight[MAX_SPOT_LIGHTS] g_SpotLights;
     DirLight[MAX_DIR_LIGHTS] g_DirLights;
-};
-
-struct Material {
-    vec3 Albedo;
-    float Opacity;
-    vec3 Normal;
-    float Roughness;
-    float Metallic;
-    float AmbientOcclusion;
-};
-
-struct BasicMaterial {
-    vec3 Diffuse;
-    float Opacity;
-    vec3 Normal;
-    float Specular;
-    float Gloss;
 };
---
+++ resources/shaders/common/material.glsl
@@ -0,0 +1,16 @@
+﻿struct Material {
+    vec3 Albedo;
+    float Opacity;
+    vec3 Normal;
+    float Roughness;
+    float Metallic;
+    float AmbientOcclusion;
+};
+
+struct BasicMaterial {
+    vec3 Diffuse;
+    float Opacity;
+    vec3 Normal;
+    float Specular;
+    float Gloss;
+};
