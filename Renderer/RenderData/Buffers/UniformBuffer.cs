41a278d2933c2c747841d11c461c4acb24dae0fd
Doctor Gurke <docgurk@gmail.com>
2022-06-18 12:02:37 +0200

    initial uniform buffer builder pattern

---
+++ Renderer/RenderData/Buffers/UniformBuffer.cs
@@ -0,0 +1,34 @@
+﻿namespace Vanadium.Renderer.RenderData.Buffers;
+
+public class UniformBuffer
+{
+	public readonly int Handle;
+	public readonly string Name;
+
+	public static HashSet<UniformBuffer> All = new HashSet<UniformBuffer>();
+
+	public UniformBuffer( int handle, string name )	{
+		Handle = handle;
+		Name = name;
+	}
+
+	/// <summary>
+	/// Start a buffer declaration with a given buffer handle name.
+	/// </summary>
+	/// <param name="name">The name of the buffer handle.</param>
+	/// <returns>A UniformBufferBuilder instance, used to construct the UniformBuffer object.</returns>
+	public static UniformBufferBuilder DeclareBuffer( string name )
+	{
+		UniformBufferBuilder builder = new UniformBufferBuilder( name );
+		return builder;
+	}
+
+	/// <summary>
+	/// Initializes the buffer's data.
+	/// </summary>
+	/// <exception cref="NotImplementedException"></exception>
+	public void Initialize()
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ Renderer/RenderData/Buffers/UniformBufferBuilder.cs
@@ -0,0 +1,33 @@
+﻿namespace Vanadium.Renderer.RenderData.Buffers;
+
+public class UniformBufferBuilder
+{
+	private readonly string Name;
+
+	public UniformBufferBuilder( string name )
+	{
+		Name = name;
+	}
+
+	public UniformBufferBuilder AddField<T>( string identifier )
+	{
+		return this;
+	}
+
+	/// <summary>
+	/// Builds and initializes the builder's UniformBuffer object.
+	/// </summary>
+	/// <returns>An instance of the UniformBuffer object.</returns>
+	public UniformBuffer Build()
+	{
+		// create empty buffer handle
+		GLUtil.CreateBuffer( Name, out var handle );
+		var buffer = new UniformBuffer( handle, Name );
+
+		// add to static hashset so we can keep track of it
+		UniformBuffer.All.Add( buffer );
+		// init buffer
+		buffer.Initialize();
+		return buffer;
+	}
+}
