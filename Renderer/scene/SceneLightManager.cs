45ce79542c19762f3d8b9e626b445bec80a8c000
Doctor Gurke <docgurk@gmail.com>
2022-06-20 16:07:13 +0200

    use Lists for lights in the light manager and pass as array to uniform buffer

--- Renderer/scene/SceneLightManager.cs
+++ Renderer/scene/SceneLightManager.cs
@@ -8,9 +8,9 @@ public class SceneLightManager
 	private int NumPointLights;
 	private int NumSpotLights;
 	private int NumDirLights;
-	private readonly PointLight[] PointLights;
-	private readonly SpotLight[] SpotLights;
-	private readonly DirLight[] DirLights;
+	private readonly List<PointLight> PointLights = new();
+	private readonly List<SpotLight> SpotLights = new();
+	private readonly List<DirLight> DirLights = new();
 
 	// Values need to match common.glsl
 	public static int MaxPointLights => 128;
@@ -54,13 +54,6 @@ public class SceneLightManager
 		public float Pad;
 	}
 
-	public SceneLightManager()
-	{
-		PointLights = new PointLight[MaxPointLights];
-		SpotLights = new SpotLight[MaxSpotLights];
-		DirLights = new DirLight[MaxDirLights];
-	}
-
 	public void SetAmbientLightColor( Color col )
 	{
 		AmbientLightColor = col.WithAlpha( 1.0f );
@@ -74,14 +67,14 @@ public class SceneLightManager
 
 	public void AddPointlight( Vector3 position, Color color, float constant = 0.0f, float linear = 0.0f, float quadratic = 1.0f, float brightness = 1.0f )
 	{
-		var light = NumPointLights; // current number is index for new light (ie, 0 lights means insert at index 0)
-		if ( light >= MaxPointLights )
+		var index = NumPointLights; // current number is index for new light (ie, 0 lights means insert at index 0)
+		if ( index >= MaxPointLights )
 		{
 			Log.Info( $"UNABLE TO ADD MORE POINT LIGHTS {MaxPointLights}" );
 			return;
 		}
 
-		Log.Info( $"new pointlight {light} {position} {color} {constant} {linear} {quadratic}" );
+		Log.Info( $"new pointlight {index} {position} {color} {constant} {linear} {quadratic}" );
 		var lightmodel = new SceneObject
 		{
 			Model = Model.Primitives.Sphere,
@@ -90,15 +83,19 @@ public class SceneLightManager
 		};
 		lightmodel.RenderColor = color;
 
-		PointLights[light].Position = position;
-		PointLights[light].Color = new Vector3( color.r, color.g, color.b );
-		PointLights[light].Constant = constant;
-		PointLights[light].Linear = linear;
-		PointLights[light].Quadratic = quadratic;
-		PointLights[light].Brightness = brightness;
+		// init and add light
+		var light = new PointLight();
+		light.Position = position;
+		light.Color = new Vector3( color.r, color.g, color.b );
+		light.Constant = constant;
+		light.Linear = linear;
+		light.Quadratic = quadratic;
+		light.Brightness = brightness;
+		PointLights.Add( light );
+
 		NumPointLights++;
 		// update whole buffer for now, this should use sub data later on
-		UniformBufferManager.Current?.UpdatePointlights( PointLights, NumPointLights );
+		UniformBufferManager.Current?.UpdatePointlights( PointLights.ToArray(), NumPointLights );
 	}
 
 	public void AddSpotlight( Vector3 position, Rotation rotation )
@@ -113,14 +110,14 @@ public class SceneLightManager
 
 	public void AddSpotlight( Vector3 position, Rotation rotation, Color color, float innerangle, float outerangle, float constant = 0.0f, float linear = 0.0f, float quadratic = 1.0f, float brightness = 1.0f )
 	{
-		var light = NumSpotLights; // current number is index for new light (ie, 0 lights means insert at index 0)
-		if ( light >= MaxSpotLights )
+		var index = NumSpotLights; // current number is index for new light (ie, 0 lights means insert at index 0)
+		if ( index >= MaxSpotLights )
 		{
 			Log.Info( $"UNABLE TO ADD MORE POINT LIGHTS {MaxSpotLights}" );
 			return;
 		}
 
-		Log.Info( $"new spotlight {light} {position} {rotation.Forward} {color} {innerangle} {outerangle} {constant} {linear} {quadratic}" );
+		Log.Info( $"new spotlight {index} {position} {rotation.Forward} {color} {innerangle} {outerangle} {constant} {linear} {quadratic}" );
 		var lightmodel = new SceneObject
 		{
 			Model = Model.Primitives.ForwardCone,
@@ -130,18 +127,22 @@ public class SceneLightManager
 		};
 		lightmodel.RenderColor = color;
 
-		SpotLights[light].Position = position;
-		SpotLights[light].Direction = rotation.Forward;
-		SpotLights[light].Color = new Vector3( color.r, color.g, color.b );
-		SpotLights[light].Constant = constant;
-		SpotLights[light].Linear = linear;
-		SpotLights[light].Quadratic = quadratic;
-		SpotLights[light].Brightness = brightness;
-		SpotLights[light].InnerAngle = innerangle.DegreeToRadian();
-		SpotLights[light].OuterAngle = outerangle.DegreeToRadian();
+		// init and add light
+		var light = new SpotLight();
+		light.Position = position;
+		light.Direction = rotation.Forward;
+		light.Color = new Vector3( color.r, color.g, color.b );
+		light.Constant = constant;
+		light.Linear = linear;
+		light.Quadratic = quadratic;
+		light.Brightness = brightness;
+		light.InnerAngle = innerangle.DegreeToRadian();
+		light.OuterAngle = outerangle.DegreeToRadian();
+		SpotLights.Add( light );
+
 		NumSpotLights++;
 		// update whole buffer for now, this should use sub data later on
-		UniformBufferManager.Current?.UpdateSpotlights( SpotLights, NumSpotLights );
+		UniformBufferManager.Current?.UpdateSpotlights( SpotLights.ToArray(), NumSpotLights );
 	}
 
 	public void AddDirLight( Rotation rotation )
@@ -151,14 +152,14 @@ public class SceneLightManager
 
 	public void AddDirLight( Rotation rotation, Color color, float brightness = 1.0f )
 	{
-		var light = NumDirLights; // current number is index for new light (ie, 0 lights means insert at index 0)
-		if ( light >= MaxDirLights )
+		var index = NumDirLights; // current number is index for new light (ie, 0 lights means insert at index 0)
+		if ( index >= MaxDirLights )
 		{
 			Log.Info( $"UNABLE TO ADD MORE DIRECTIONAL LIGHTS {MaxDirLights}" );
 			return;
 		}
 
-		Log.Info( $"new dirlight {light} {rotation.Forward} {color}" );
+		Log.Info( $"new dirlight {index} {rotation.Forward} {color}" );
 		var lightmodel = new SceneObject
 		{
 			Model = Model.Primitives.ForwardCone,
@@ -168,10 +169,14 @@ public class SceneLightManager
 		};
 		lightmodel.RenderColor = color;
 
-		DirLights[light].Direction = rotation.Forward;
-		DirLights[light].Color = new Vector3( color.r, color.g, color.b );
-		DirLights[light].Brightness = brightness;
+		// init and add light
+		var light = new DirLight();
+		light.Direction = rotation.Forward;
+		light.Color = new Vector3( color.r, color.g, color.b );
+		light.Brightness = brightness;
+		DirLights.Add( light );
+
 		NumDirLights++;
-		UniformBufferManager.Current?.UpdateDirlights( DirLights, NumDirLights );
+		UniformBufferManager.Current?.UpdateDirlights( DirLights.ToArray(), NumDirLights );
 	}
 }
